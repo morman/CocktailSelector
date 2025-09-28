345cd1378bf73f20128a67c42ea2ec025db4eef3
morman <stephen.morman@gmail.com>
2025-09-28 13:14:57 -0500

    Extended Ingredient to be a pair.

--- src/main/java/com/morman/cocktailselector/model/Ingredient.java
+++ src/main/java/com/morman/cocktailselector/model/Ingredient.java
@@ -1,26 +1,42 @@
 package com.morman.cocktailselector.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import java.util.Objects;
 
 @Embeddable
 public class Ingredient {
 
-    private String name;
+    // e.g., "2 oz", "1 tsp"; keep as String for flexibility
+    @Column(name = "quantity")
+    private String quantity;
+
+    // the ingredient name itself, e.g., "Vodka", "Lime Juice"
+    @Column(name = "element")
+    private String element;
 
     public Ingredient() {
     }
 
-    public Ingredient(String name) {
-        this.name = name;
+    public Ingredient(String quantity, String element) {
+        this.quantity = quantity;
+        this.element = element;
     }
 
-    public String getName() {
-        return name;
+    public String getQuantity() {
+        return quantity;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getElement() {
+        return element;
+    }
+
+    public void setElement(String element) {
+        this.element = element;
     }
 
     @Override
@@ -28,11 +44,17 @@ public class Ingredient {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Ingredient that = (Ingredient) o;
-        return Objects.equals(name, that.name);
+        return Objects.equals(quantity, that.quantity) &&
+               Objects.equals(element, that.element);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name);
+        return Objects.hash(quantity, element);
+    }
+
+    @Override
+    public String toString() {
+        return (quantity != null ? quantity + " " : "") + (element != null ? element : "");
     }
 }
